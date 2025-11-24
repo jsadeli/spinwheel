521c1be1b4f52899a85c1d17e61519d6bb7a6565
Jeffrey Sadeli <jsadeli@users.noreply.github.com>
2025-11-24 13:22:28 +0700

    feat: add PWA support with manifest and service worker

---
+++ sw.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = 'spinwheel-v1';
+const ASSETS = [
+    './',
+    './index.html',
+    './icon.png',
+    './manifest.json',
+    'https://cdn.tailwindcss.com',
+    'https://unpkg.com/react@18/umd/react.production.min.js',
+    'https://unpkg.com/react-dom@18/umd/react-dom.production.min.js',
+    'https://unpkg.com/@babel/standalone/babel.min.js'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(ASSETS);
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cache) => {
+                    if (cache !== CACHE_NAME) {
+                        return caches.delete(cache);
+                    }
+                })
+            );
+        })
+    );
+});
