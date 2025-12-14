553d26b85cb183b6ad9b188e03500464f28c30cc
Jeffrey Sadeli <jsadeli@users.noreply.github.com>
2025-12-14 16:45:02 +0700

    chore: add global icon type definitions

--- global.d.ts
+++ global.d.ts
@@ -4,12 +4,69 @@ declare global {
   interface Window {
     ThemeIcon: React.FC<any>;
     Modal: React.FC<any>;
-    // Add other globals here as you encounter errors
+    // Icons
+    Icon: React.FC<any>;
+    SpinWheelLogo: React.FC<any>;
+    SpinCoinLogo: React.FC<any>;
+    SettingsIcon: React.FC<any>;
+    SlidersIcon: React.FC<any>;
+    Volume2Icon: React.FC<any>;
+    VolumeXIcon: React.FC<any>;
+    ShuffleIcon: React.FC<any>;
+    Trash2Icon: React.FC<any>;
+    TrophyIcon: React.FC<any>;
+    SunIcon: React.FC<any>;
+    MoonIcon: React.FC<any>;
+    MonitorIcon: React.FC<any>;
+    CheckIcon: React.FC<any>;
+    ChevronDownIcon: React.FC<any>;
+    GithubIcon: React.FC<any>;
+    ShareIcon: React.FC<any>;
+    CopyIcon: React.FC<any>;
+    ClockIcon: React.FC<any>;
+    EyeIcon: React.FC<any>;
+    EyeOffIcon: React.FC<any>;
+    ZapIcon: React.FC<any>;
+    HourglassIcon: React.FC<any>;
+    CodeIcon: React.FC<any>;
+    ScrollIcon: React.FC<any>;
+    HelpCircleIcon: React.FC<any>;
+    SparklesIcon: React.FC<any>;
+    BotIcon: React.FC<any>;
+    KeyIcon: React.FC<any>;
+    PaletteIcon: React.FC<any>;
+    PlusIcon: React.FC<any>;
+    DownloadIcon: React.FC<any>;
+    TrendingUpIcon: React.FC<any>;
+    CloseIcon: React.FC<any>;
+    CirclePlusIcon: React.FC<any>;
+    TrailIcon: React.FC<any>;
+    MusicIcon: React.FC<any>;
+    ElectricIcon: React.FC<any>;
+    WoodIcon: React.FC<any>;
+    MechanicalIcon: React.FC<any>;
+    CrystalGlassIcon: React.FC<any>;
+    AlertTriangleIcon: React.FC<any>;
+    SkullIcon: React.FC<any>;
+    MaleIcon: React.FC<any>;
+    FemaleIcon: React.FC<any>;
+    SwordsIcon: React.FC<any>;
+    ShoppingBagIcon: React.FC<any>;
+    StarIcon: React.FC<any>;
+    SeedlingIcon: React.FC<any>;
+    BoltIcon: React.FC<any>;
+    FlameIcon: React.FC<any>;
+    GemIcon: React.FC<any>;
+    CrownIcon: React.FC<any>;
+    PlayIcon: React.FC<any>;
+    StopIcon: React.FC<any>;
+    // Themes
     THEMES: {
       AUTO: string;
       LIGHT: string;
       DARK: string;
     };
+    // Achievements
     AchievementFilters: {
       ALL: string;
       UNLOCKED: string;
